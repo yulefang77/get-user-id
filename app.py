6416aed4a62dafea993a9620e43fef78f3ee2e05
yule <yulefang77@gmail.com>
2024-05-03 23:28:33 +0800

    refeactor

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, request, abort
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
@@ -14,6 +15,9 @@ from linebot.v3.webhooks import (
     TextMessageContent
 )
 
+# 加載 .env 文件中的環境變量
+load_dotenv()
+
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
 
@@ -25,14 +29,14 @@ handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # Get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
+    # Get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
+    # Handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -43,28 +47,35 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessageContent)
-def handle_message(event):   
-    # Enter a context with an instance of the API client
+def handle_message(event):
     with ApiClient(configuration) as api_client:
-        # Create an instance of the API class
         line_bot_api = MessagingApi(api_client)
-        user_id = event.source.user_id       
+        user_id = event.source.user_id
 
-        try:
-            profile = line_bot_api.get_profile(user_id)
-            user_name = profile.display_name
-            print("使用者名稱:" + user_name)
-        except Exception as e:
-            print("Exception when calling MessagingApi->get_profile: %s\n" % e)
+        if event.source.type == 'group':
+            try:
+                profile = line_bot_api.get_group_member_profile(event.source.group_id, user_id)
+                user_name = profile.display_name
+                user_text = "我是" + user_name + ", 我們在群組裡。"
+                print(user_text)
+            except Exception as e:
+                print("Exception when calling MessagingApi->get_profile: %s\n" % e)
+        else:
+            try:
+                profile = line_bot_api.get_profile(user_id)
+                user_name = profile.display_name
+                user_text = "我是" + user_name + ", 我不在群組裡。"
+                print(user_text)
+            except Exception as e:
+                print("Exception when calling MessagingApi->get_profile: %s\n" % e)
 
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text=user_name)]
+                messages=[TextMessage(text=user_text)]
             )
         )
 
+
 if __name__ == "__main__":
     app.run()
-
-# https://github.com/line/line-bot-sdk-python/blob/master/linebot/v3/messaging/docs/MessagingApi.md#get_profile
